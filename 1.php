7032d64f84c4faf1e0e66fd7fa31dca93371883b
Prinu Vinod Nair <71308351+PrinuVinod@users.noreply.github.com>
2023-12-18 09:10:05 +0530

    1st Commit

---
+++ 1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        // echo "hello world";
+        $person = array(
+        "first_name" => "John",
+        "last_name" => "Doe",
+        "age" => 30
+        );
+    ?>    
+</body>
+</html>
---
+++ php.php
@@ -0,0 +1,11 @@
+<?php
+$string1 = 'first string';
+$string2 = 'second string';
+ 
+if ($string1 == $string2) {
+	echo 'Strings are equal';
+}
+else {
+	echo 'Strings are not equal';
+}
+?>
